a9c16c0cdea972181ffae90faf310475cf8bb45b
Anniestannie <158219149+Anniestannie@users.noreply.github.com>
2024-04-27 21:04:41 -0400

    heart

--- Senior_Project/main.js
+++ Senior_Project/main.js
@@ -4,9 +4,7 @@ const canvas = document.getElementById('canvas3d');
 
 const spline = new Application(canvas);
 // spline.load('https://prod.spline.design/aQE0zOgRV8XlSncK/scene.splinecode');
-
 spline.load('https://prod.spline.design/aQE0zOgRV8XlSncK/scene.splinecode')
-
 
 
 function updateTime() {
@@ -18,61 +16,44 @@ function updateTime() {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     
     let thisDate = today.getDate();
-    
     let thisWeekday = today.getDay();
     
     let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     
-    console.log(weekdays[thisWeekday]);
-    
     let thisYear = today.getFullYear();
-    console.log(thisYear);
     
     let dateElement = document.getElementById("dateHere");
     
     dateElement.innerHTML = months[thisMonth] + " " + thisDate + ", " + thisYear;
     
-    let thisHour = today.getHours();
-    
+    let thisHour = today.getHours() % 12;
     let thisMinute = today.getMinutes();
-    
     let thisSecond = today.getSeconds();
-    console.log(thisSecond);
       
-    if(thisHour > 12) {
-      thisHour = thisHour - 12
-    }
-      
-    if(thisMinute < 10) {
+    if(thisMinute < 10)
       thisMinute = "0" + thisMinute;
-    }
       
-    if(thisSecond < 10) {
+    if(thisSecond < 10) 
       thisSecond = "0" + thisSecond;
-    }
-      
+
     let timeElement = document.getElementById("timeHere");
     
     timeElement.innerHTML = thisHour + ":" + thisMinute + ":" + thisSecond;
-    }
+}
     
-    setInterval(updateTime, 1000);
-
-
+setInterval(updateTime, 1000);
 
 let startButton = document.getElementById('startbutton');
 let loadingScreen = document.getElementById('loadingscreen');
     
 
-    setTimeout(function() {
-      startButton.style.display = 'block'; 
-    }, ); 
-    
-    startButton.addEventListener('click', function() {
-      loadingScreen.style.display = 'none';
-    });
+setTimeout(function() {
+    startButton.style.display = 'block'; 
+}); 
 
-
+startButton.addEventListener('click', function() {
+    loadingScreen.style.display = 'none';
+});
 
 let instructionScreen = document.getElementById('instructionScreen');
 let closeButton = document.getElementById('closeInstructions');
---
+++ Senior_Project/past.js
@@ -0,0 +1,17 @@
+let gridContainer = document.querySelector('.grid-container');
+let gridItems = document.querySelectorAll('.grid-item');
+
+function adjustGrid() {
+    const maxRows = 5;
+    let currentRow = 0;
+
+    for (let i = 0; i < gridItems.length; i++) {
+        if (i !== 0 && i % maxRows === 0) { 
+            currentRow++;
+        }
+        gridItems[i].style.gridRow = `${currentRow + 1}`;
+    }
+}
+
+adjustGrid();
+
