201baa72321749ba795e9f7cef8bdb21af54bcd7
Anniestannie <158219149+Anniestannie@users.noreply.github.com>
2024-05-02 02:19:22 -0400

    ddd

--- heartscapes/main.js
+++ heartscapes/main.js
@@ -38,7 +38,7 @@ const canvas = document.getElementById('canvas3d');
 
 const spline = new Application(canvas);
 // spline.load('https://prod.spline.design/aQE0zOgRV8XlSncK/scene.splinecode');
-spline.load('https://prod.spline.design/fQJpdWeyTEMmz1yG/scene.splinecode');
+spline.load('https://prod.spline.design/G0IE-dgDKD0eCn8p/scene.splinecode');
 
 
 
