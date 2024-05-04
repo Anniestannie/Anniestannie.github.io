f83d0151b2b4ff8f69c155dc09b724fbc35552c0
Anniestannie <158219149+Anniestannie@users.noreply.github.com>
2024-05-04 01:37:03 -0400

    new

--- heartscapes/main.js
+++ heartscapes/main.js
@@ -38,7 +38,7 @@ const canvas = document.getElementById('canvas3d');
 
 const spline = new Application(canvas);
 // spline.load('https://prod.spline.design/aQE0zOgRV8XlSncK/scene.splinecode');
-spline.load('https://prod.spline.design/bPu6K5VTdBOsNCjo/scene.splinecode');
+spline.load('https://prod.spline.design/8so1Ys8Fl1hOXimu/scene.splinecode');
 
 
 let startButton = document.getElementById('startbutton');
---
+++ heartscapes/past_pages/may3.js
@@ -0,0 +1,7 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+
+const spline = new Application(canvas);
+
+spline.load('https://prod.spline.design/bPu6K5VTdBOsNCjo/scene.splinecode');
